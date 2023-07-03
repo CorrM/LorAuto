74f6bb19df22f21e303ff37a833283f2f32a3de2
CorrM <islamnofl.official@gmail.com>
2023-07-03 08:18:29 +0300

    Add logger

--- src/LorAuto.Cli/Program.cs
+++ src/LorAuto.Cli/Program.cs
@@ -2,33 +2,63 @@
 using LorAuto.Client;
 using LorAuto.Game;
 using LorAuto.Strategies;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
 
-// TODO: Before any keyboard press, check if game are foreground, maybe for mouse too
+// # Logger
+Log.Logger = new LoggerConfiguration()
+#if DEBUG
+    .MinimumLevel.Debug()
+#endif
+    .WriteTo.Console()
+    .CreateLogger();
 
+// # Terminate handle
+var cts = new CancellationTokenSource();
+Console.CancelKeyPress += (_, eventArgs) =>
+{
+    Log.Logger.Warning("Please note that the application's shutdown process has been initiated and may take a few moments to complete");
+    eventArgs.Cancel = true;
+    cts.Cancel();
+};
+
+// # Card sets
 var cardSetsManager = new CardSetsManager("CardSets");
 
-Console.Write("Downloading missing card sets ... ");
+Log.Logger.Information("Downloading missing card sets starts");
 //await cardSetsManager.DownloadMissingCardSetsAsync().ConfigureAwait(false);
-Console.WriteLine("Done");
+Log.Logger.Information("Downloading missing card sets finished");
 
-Console.Write("Load card sets ... ");
+Log.Logger.Information("Loading card sets");
 await cardSetsManager.LoadCardSetsAsync().ConfigureAwait(false);
-Console.WriteLine("Done");
+Log.Logger.Information("Loading card sets finished");
 
+// Game info and data
 using var gameClientApi = new GameClientApi();
 var stateMachine = new StateMachine(cardSetsManager, gameClientApi);
 
 stateMachine.UpdateClientInfo();
-await stateMachine.UpdateGameDataAsync().ConfigureAwait(false);
+
 if (stateMachine.GameWindowHandle == IntPtr.Zero)
 {
-    Console.WriteLine("Legends of Runeterra isn't running!");
+    Log.Logger.Error("Legends of Runeterra isn't running!");
     return -1;
 }
 
-var bot = new Bot(stateMachine, new Generic(), GameRotationType.Standard, false);
+await stateMachine.UpdateGameDataAsync(cts.Token).ConfigureAwait(false);
 
-while (true)
-    await bot.ProcessAsync().ConfigureAwait(false);
+// BOT
+ILoggerFactory loggerFactory = new SerilogLoggerFactory(Log.Logger);
+var bot = new Bot(stateMachine, new Generic(), GameRotationType.Standard, false, loggerFactory.CreateLogger<Bot>());
+
+while (!cts.IsCancellationRequested)
+{
+    await bot.ProcessAsync(cts.Token).ConfigureAwait(false);
+    await Task.Delay(8).ConfigureAwait(false);
+}
+
+// Clean
+Log.CloseAndFlush();
 
 return 0;
